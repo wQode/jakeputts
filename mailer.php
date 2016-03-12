eda988cad1b4d832ba56f43d66ff74cf7724292d
wque <wqueit@gmail.com>
2016-03-12 21:53:01 +0800

    update php mail

--- mailer.php
+++ mailer.php
@@ -2,19 +2,7 @@
 
     // Only processing POST reqeusts
     if (isset($_REQUEST['email'])) {
-        // Retrieving the form fields and removing whitespace
-        $name = strip_tags(trim($_POST["name"]));
-        $name = str_replace(array("\r","\n"),array(" "," "),$name);
-        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-        $message = trim($_POST["message"]);
 
-        // Checking if data sent to mailer
-        if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            // Setting a 400 (bad request) response code and exit.
-            http_response_code(400);
-            echo "Oops! There was a problem with your submission. Please complete the form and try again.";
-            exit;
-        }
 
         // Receipient's email address
         $recipient = "jakeputts@gmail.com";
@@ -29,16 +17,14 @@
 
     
         // Sending the email
-        mail($recipient, "$subject", $email_content, "From:" . $email);
+        mail($recipient, "$subject", $message, "From:" . $email);
             // Setting a 200 (okay) response code.
         http_response_code(200);
         echo "Thank You! Your message has been sent.";
         } 
 
         else {
-            // Setting a 500 (internal server error) response code.
-            http_response_code(500);
-            echo "Oops! Something went wrong and we couldn't send your message.";
+      
 ?>
 
 <form method="post">
