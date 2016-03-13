ac5118202e203b4bfc494e11bd105aea2879db03
wque <wqueit@gmail.com>
2016-03-13 10:13:03 +0800

    update php mail

---
+++ mailer_backup1.php
@@ -0,0 +1,14 @@
+<?php
+        $recipient = "jakeputts@gmail.com";
+        $name = $_POST['name']; 
+        $email = $_POST['email'];
+        $message = $_POST['message'];
+        $subject = "Email for Jake";
+        $headers = "From: $name \r\n";
+?>
+
+<?php    
+        // Sending the email
+        mail($recipient, $subject, $message, $headers);
+        echo "Thank You! Your message has been sent.";
+?>
