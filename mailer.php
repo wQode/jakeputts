ccc98da0fd599cda3ac23ecf1ca7958892fd8404
wque <wqueit@gmail.com>
2016-03-13 10:30:27 +0800

    update php mail

--- mailer.php
+++ mailer.php
@@ -1,7 +1,7 @@
 <?php
 
     // Only processing POST reqeusts
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  /* if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Retrieving the form fields and removing whitespace
         $name = strip_tags(trim($_POST["name"]));
         $name = str_replace(array("\r","\n"),array(" "," "),$name);
@@ -15,7 +15,9 @@
             echo "Oops! There was a problem with your submission. Please complete the form and try again.";
             exit;
         }
+        */
 
+    if ($_POST["message"]) {
         // Receipient's email address
         $recipient = "jakeputts@gmail.com";
 
@@ -25,27 +27,27 @@
         // Building the email content
         //$email_content = "Name: $name\n";
         //$email_content .= "Email: $email\n\n";
-        $email_content .= "Message:\n$message\n";
+        $email_content = "Message:\n$message\n";
 
         // Building the email headers
         $email_headers = "From: $name <$email>";
 
         // Sending the email
-        if ($_POST["message"]) {
-            mail($recipient, $subject, $email_content, $email_headers)) 
+        mail($recipient, $subject, $email_content, $email_headers)) 
             // Setting a 200 (okay) response code.
             http_response_code(200);
             echo "Thank You! Your message has been sent.";
-        } else {
+    } else {
             // Setting a 500 (internal server error) response code.
             http_response_code(500);
             echo "Oops! Something went wrong and we couldn't send your message.";
         }
-
+    /*
     } else {
         // Setting  a 403 (forbidden) response code if not a POST request
         http_response_code(403);
         echo "There was a problem with your submission, please try again.";
     }
+    */
 
 ?>
