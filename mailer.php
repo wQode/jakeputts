bccd6ef82b330b56d253abffa445698f763a9e0a
wque <wqueit@gmail.com>
2016-03-13 10:52:06 +0800

    update php mail

--- mailer.php
+++ mailer.php
@@ -27,10 +27,12 @@
         // Building the email content
         //$email_content = "Name: $name\n";
         //$email_content .= "Email: $email\n\n";
-        $email_content = "Message:\n$message\n";
+        //$email_content = "Message:\n$message\n";
+        $email_content = $message;
 
         // Building the email headers
-        $email_headers = "From: $name <$email>";
+        //$email_headers = "From: $name <$email>";
+        $email_headers = "From: $name";
 
         // Sending the email
         mail($recipient, $subject, $email_content, $email_headers); 
@@ -38,7 +40,7 @@
             http_response_code(200);
             echo "Thank You! Your message has been sent.";
     //} 
-    else {
+    /*else {
             // Setting a 500 (internal server error) response code.
             http_response_code(500);
             echo "Oops! Something went wrong and we couldn't send your message.";
