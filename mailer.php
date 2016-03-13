506a6505f135fa513732d8f1350c31cc0c7729a4
wque <wqueit@gmail.com>
2016-03-13 10:00:44 +0800

    update php mail

--- mailer.php
+++ mailer.php
@@ -5,10 +5,12 @@
         $name = $_POST['name']; 
         $email = $_POST['email'];
         $message = $_POST['message'];
+        $subject = "Email for Jake";
+        $headers = "From: $name \r\n";
 ?>
 
 <?php    
         // Sending the email
-        mail($recipient, "Mail for Jake Putts", $message, "From:" . $email);
+        mail($recipient, $subject, $message, $headers);
         echo "Thank You! Your message has been sent.";
 ?>
