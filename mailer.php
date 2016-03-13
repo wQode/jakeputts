c34c78b4aac76627c04d67c7fde0a6ec665dd7df
wque <wqueit@gmail.com>
2016-03-13 09:42:35 +0800

    update php mail

--- mailer.php
+++ mailer.php
@@ -1,40 +1,14 @@
 <?php
 
-    // Only processing POST reqeusts
-    if (isset($_REQUEST['email'])) {
-
-
-        // Receipient's email address
+     
         $recipient = "jakeputts@gmail.com";
-
-        $email = $_REQUEST['email'];
-
-        // Setting the email subject
-        $subject = $_REQUEST['subject'];
-
-        // Building the email content
-        $message = $_REQUEST['message'];
-
-    
-        // Sending the email
-        mail($recipient, "$subject", $message, "From:" . $email);
-            // Setting a 200 (okay) response code.
-        http_response_code(200);
-        echo "Thank You! Your message has been sent.";
-        } 
-
-        else {
-      
+        $name = $_POST['name']
+        $email = $_POST['email'];
+        $message = $_POST['message'];
 ?>
 
-<form method="post">
- Email: <input name="email" type="text" /><br />
- Subject: <input name="subject" type="text" /><br />
- Message:<br />
- <textarea name="message" rows="15" cols="40"></textarea><br />
- <input type="submit" value="Submit" />    
- </form>
-
- <?php 
-  }
+<?php    
+        // Sending the email
+        mail($recipient, "Mail for Jake Putts", $message, "From:" . $email);
+        echo "Thank You! Your message has been sent.";
 ?>
