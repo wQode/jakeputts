ad7b489af4c9304800579cd510cfcb64be19dd43
wque <wqueit@gmail.com>
2016-03-14 18:37:07 +0800

    update php mail

--- mailer.php
+++ mailer.php
@@ -21,31 +21,8 @@ if(isset($_POST['submit'])){
             "subject" => $subject,
             "text"    => $msg
         ));
+http_response_code(200);
 echo "<script>alert('Email sent.');</script>";
 } 
-/*
-    
-# Include the Autoloader (see "Libraries" for install instructions)
-require 'vendor/autoload.php';
-use Mailgun\Mailgun;
 
-if(isset($_POST['submit'])){
-    $name = $_POST['name']; 
-    $email = $_POST['email']; 
-    $msg = $_POST['message'];
-    $subject = "Contact from wqode.space";    
-
-    # Instantiate the client.
-    $mgClient = new Mailgun("key-aa3fb247e126efcd34632167d6633e63");
-    $domain = "wqode.space";
-
-    $result = $mgClient->sendMessage($domain, array(
-            "from"    => "$name <mailgun@wqode.space>",
-            "to"      => $email,
-            "subject" => $subject,
-            "text"    => $msg
-        ));
-echo "<script>alert('Email sent.');</script>";
-} 
-*/
 ?>
