0363f3aacc33cf3649755e3952c5bc45d95853cc
wque <wqueit@gmail.com>
2016-03-14 18:00:27 +0800

    update php mail

--- mailer.php
+++ mailer.php
@@ -20,38 +20,27 @@ function send_mail($email, $subject, $msg) {
     return $result; 
 }
 */
-
+    
 # Include the Autoloader (see "Libraries" for install instructions)
 require 'vendor/autoload.php';
 use Mailgun\Mailgun;
-
-# Instantiate the client.
-$mgClient = new Mailgun("pubkey-bf4d623ad021b625f38bee21e8fb305b");
-$domain = "wqode.space";
-
-
 
 if(isset($_POST['submit'])){
     $name = $_POST['name']; 
     $email = $_POST['email']; 
     $msg = $_POST['message'];
     $subject = "Contact from wqode.space";    
-    if($name !="" && $msg != ""){
-        # Make the call to the client.
-        $mg->sendMessage($domain, array(
-            'from'    => $name,
-            'to'      => $email,
-            'subject' => $subject,
-            'text'    => $msg
-        ));
 
-/*
-        $ip_address = $_SERVER['REMOTE_ADDR'];
-        send_mail("wqodes@gmail.com", "New Message from wqode.space","The IP ($ip_address) has sent you a message: <blockquote>$msg</blockquote>");
-*/
-        echo "<h2 style='color:green;'>Your message Has Been Sent</h2>";
-    }else{
-        echo "<h2 style='color:red;'>Please check all fields</h2>";
-    }
+    # Instantiate the client.
+    $mgClient = new Mailgun("pubkey-bf4d623ad021b625f38bee21e8fb305b");
+    $domain = "wqode.space";
+
+    $result = $mgClient->sendMessage($domain, array(
+            "from"    => "$name <mailgun@wqode.space>",
+            "to"      => $email,
+            "subject" => $subject,
+            "text"    => $msg
+        ));
+echo "<script>alert('Email Sent Successfully.. !!');</script>";
 } 
 ?>
